8245cbe9b9e4853bc7aaead5a98bc48f69f13221
Andria <andria.inas@gmail.com>
2024-01-05 22:18:35 +0400

    first commit

---
+++ MovieTracker/MovieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackerApp.swift
+//  MovieTracker
+//
+//  Created by Andria Inasaridze on 05.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
