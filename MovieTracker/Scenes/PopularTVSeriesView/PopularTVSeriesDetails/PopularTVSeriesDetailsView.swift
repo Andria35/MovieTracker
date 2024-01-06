eca91d5a0f08e096aa4282a1bb748befb2333897
luk407 <luka.gazdeliani07@gmail.com>
2024-01-06 14:55:58 +0400

    add content to PopularTVSeriesDetailsView

--- MovieTracker/Scenes/PopularTVSeriesView/PopularTVSeriesDetails/PopularTVSeriesDetailsView.swift
+++ MovieTracker/Scenes/PopularTVSeriesView/PopularTVSeriesDetails/PopularTVSeriesDetailsView.swift
@@ -9,11 +9,55 @@ import SwiftUI
 import NetworkManager
 
 struct PopularTVSeriesDetailsView: View {
+    @StateObject var popularTVSeriesDetailsViewModel: PopularTVSeriesDetailsViewModel
+    @Binding var tvSeriesID: Int
+    
     var body: some View {
-        Text("")
+        VStack(spacing: 24) {
+            Image(systemName: "photo")
+                .resizable()
+                .frame(width: .infinity, height: 456)
+                .clipShape(RoundedRectangle(cornerRadius: 72))
+            
+            HStack {
+                Text("\(popularTVSeriesDetailsViewModel.tvSeriesName)")
+                    .font(.system(size: 20))
+                    .bold()
+                
+                Spacer()
+                
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundStyle(.yellow)
+                    .frame(width: 20, height: 20)
+                
+                Text("\(popularTVSeriesDetailsViewModel.tvSeriesVoteAverage)")
+            }
+            
+            HStack {
+                Text("\(popularTVSeriesDetailsViewModel.tvSeriesReleaseDate)")
+                    .foregroundStyle(.opacity(0.5))
+                
+                Spacer()
+            }
+            
+            Text("\(popularTVSeriesDetailsViewModel.tvSeriesOverview)")
+            
+            HStack {
+                Text("Genre: \(popularTVSeriesDetailsViewModel.formatedGenreString)")
+                    .foregroundStyle(.opacity(0.5))
+                Spacer()
+            }
+            
+            HStack {
+                Text("\(popularTVSeriesDetailsViewModel.tvSeriesNumberOfSeasons)")
+                    .foregroundStyle(.opacity(0.5))
+                Spacer()
+            }
+        }
     }
 }
 
-#Preview {
-    PopularTVSeriesDetailsView()
-}
+//#Preview {
+//    PopularTVSeriesDetailsView(popularTVSeriesDetailsViewModel: PopularTVSeriesDetailsViewModel(networkManager: NetworkManager(), tvSeriesID: 4191117), tvSeriesID: 4191117)
+//}
