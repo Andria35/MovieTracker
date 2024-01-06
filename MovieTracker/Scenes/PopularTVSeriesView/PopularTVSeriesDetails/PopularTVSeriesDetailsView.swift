8fdad33be9a4ff3ae93bd4996064601a4bb46ffd
luk407 <luka.gazdeliani07@gmail.com>
2024-01-06 18:29:41 +0400

    divide PopularTVSeriesDetailsView body into smaller parts

--- MovieTracker/Scenes/PopularTVSeriesView/PopularTVSeriesDetails/PopularTVSeriesDetailsView.swift
+++ MovieTracker/Scenes/PopularTVSeriesView/PopularTVSeriesDetails/PopularTVSeriesDetailsView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 import NetworkManager
 
 struct PopularTVSeriesDetailsView: View {
+    // MARK: Properties
     @StateObject var popularTVSeriesDetailsViewModel: PopularTVSeriesDetailsViewModel
     @EnvironmentObject private var router: Router
     
+    // MARK: - Body
     var body: some View {
         VStack(spacing: 24) {
             if popularTVSeriesDetailsViewModel.tvSeriesImageBackdropPath != "" {
@@ -20,45 +22,68 @@ struct PopularTVSeriesDetailsView: View {
             
             VStack(spacing: 12) {
                 HStack {
-                    Text("\(popularTVSeriesDetailsViewModel.tvSeriesName)")
-                        .font(.system(size: 20))
-                        .bold()
+                    NameView
                     
                     Spacer()
                     
-                    Image(systemName: "star.fill")
-                        .resizable()
-                        .frame(width: 20, height: 20)
-                        .foregroundStyle(.yellow)
-                    
-                    Text("\(String(format: "%.1f", popularTVSeriesDetailsViewModel.tvSeriesVoteAverage))")
+                    RatingView
                 }
                 
-                HStack {
-                    Text("\(popularTVSeriesDetailsViewModel.tvSeriesReleaseDate)")
-                        .foregroundStyle(.opacity(0.5))
-                    
-                    Spacer()
-                }
+                ReleaseDateView
             }
             
             Text("\(popularTVSeriesDetailsViewModel.tvSeriesOverview)")
                 .lineLimit(3...5)
             
             VStack(spacing: 12) {
-                HStack {
-                    Text("Genre: \(popularTVSeriesDetailsViewModel.formatedGenreString)")
-                        .foregroundStyle(.opacity(0.5))
-                    Spacer()
-                }
+                GenreView
                 
-                HStack {
-                    Text("Number of Seasons: \(popularTVSeriesDetailsViewModel.tvSeriesNumberOfSeasons)")
-                        .foregroundStyle(.opacity(0.5))
-                    Spacer()
-                }
+                NumberOfSeasonsView
             }
         }
         .padding()
+    }
+    
+    // MARK: - Views
+    private var NameView: some View {
+        Text("\(popularTVSeriesDetailsViewModel.tvSeriesName)")
+            .font(.system(size: 20))
+            .bold()
+    }
+    
+    private var RatingView: some View {
+        HStack {
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(.yellow)
+            
+            Text("\(String(format: "%.1f", popularTVSeriesDetailsViewModel.tvSeriesVoteAverage))")
+        }
+    }
+    
+    private var ReleaseDateView: some View {
+        HStack {
+            Text("\(popularTVSeriesDetailsViewModel.tvSeriesReleaseDate)")
+                .foregroundStyle(.opacity(0.5))
+            
+            Spacer()
+        }
+    }
+    
+    private var GenreView: some View {
+        HStack {
+            Text("Genre: \(popularTVSeriesDetailsViewModel.formatedGenreString)")
+                .foregroundStyle(.opacity(0.5))
+            Spacer()
+        }
+    }
+    
+    private var NumberOfSeasonsView: some View {
+        HStack {
+            Text("Number of Seasons: \(popularTVSeriesDetailsViewModel.tvSeriesNumberOfSeasons)")
+                .foregroundStyle(.opacity(0.5))
+            Spacer()
+        }
     }
 }
