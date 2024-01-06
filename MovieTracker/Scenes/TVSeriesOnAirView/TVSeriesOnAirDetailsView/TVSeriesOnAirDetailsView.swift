d95201632f3b2679426e797101ed851a59b979bc
Andria <andria.inas@gmail.com>
2024-01-06 18:47:54 +0400

    add TVSeriesOnAirDetails page navigation

---
+++ MovieTracker/Scenes/TVSeriesOnAirView/TVSeriesOnAirDetailsView/TVSeriesOnAirDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  TVSeriesOnAirDetailsView.swift
+//  MovieTracker
+//
+//  Created by Andria Inasaridze on 06.01.24.
+//
+
+import SwiftUI
+
+struct TVSeriesOnAirDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TVSeriesOnAirDetailsView()
+}
---
+++ MovieTracker/Scenes/TVSeriesOnAirView/TVSeriesOnAirDetailsView/TVSeriesOnAirDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TVSeriesOnAirDetailsViewModel.swift
+//  MovieTracker
+//
+//  Created by Andria Inasaridze on 06.01.24.
+//
+
+import Foundation
