18e4c971613ab355ad079e0ec38756496d94bbd4
sofoetno <sopho.machurishvili@gmail.com>
2024-01-06 15:03:17 +0400

    move popular movies grid item as separate component

---
+++ MovieTracker/Components/PopularMovies/PopularMoviesGridItemComponentView.swift
@@ -0,0 +1,37 @@
+//
+//  PopularMoviesGridItemComponentView.swift
+//  MovieTracker
+//
+//  Created by Sofo Machurishvili on 06.01.24.
+//
+
+import SwiftUI
+
+struct PopularMoviesGridItemComponentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    let movie: MovieModel
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: MovieHelper.constructFullImageUrl(imageUrl: movie.posterPath))) { phase in
+                switch phase {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                    
+                default: ProgressView()
+                }
+            }
+            
+            Text(movie.title + "\n")
+                .lineLimit(2)
+                .foregroundStyle(colorScheme == .dark ? .white : .black)
+        }
+    }
+}
+
+#Preview {
+    PopularMoviesGridItemComponentView(movie: MovieModel(id: 1, title: "Movie", posterPath: ""))
+}
--- MovieTracker/Scenes/PopularMoviesView/PopularMoviesView.swift
+++ MovieTracker/Scenes/PopularMoviesView/PopularMoviesView.swift
@@ -8,33 +8,18 @@
 import SwiftUI
 
 struct PopularMoviesView: View {
-    @EnvironmentObject var popularMoviesViewRouter: Router
     @StateObject var popularMoviesViewModel = PopularMoviesViewModel()
+    @EnvironmentObject var popularMoviesViewRouter: Router
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: [GridItem(.flexible(), spacing: 20), GridItem(.flexible(), spacing: 20)], spacing: 20, content: {
                 ForEach(popularMoviesViewModel.movies, id: \.id) { movie in
-                    VStack {
-                        AsyncImage(url: URL(string: MovieHelper.constructFullImageUrl(imageUrl: movie.posterPath))) { phase in
-                            switch phase {
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                
-                            default: ProgressView()
-                            }
+                    PopularMoviesGridItemComponentView(movie: movie)
+                        .onTapGesture {
+                            popularMoviesViewRouter.navigate(to: .movieDetails(movie.id))
                         }
-                        
-                        Text(movie.title + "\n")
-                            .lineLimit(2)
-                            .foregroundStyle(colorScheme == .dark ? .white : .black)
-                    }
-                    .onTapGesture {
-                        popularMoviesViewRouter.navigate(to: .movieDetails(movie.id))
-                    }
                 }
             })
             .padding()
