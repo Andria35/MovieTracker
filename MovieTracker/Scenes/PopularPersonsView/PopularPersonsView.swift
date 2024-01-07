ee924aa1d061758a966733066f696f8fe567c1e6
salomege <salomegelenidze@gmail.com>
2024-01-06 22:03:50 -0800

    make first page

--- MovieTracker/Scenes/PopularPersonsView/PopularPersonsView.swift
+++ MovieTracker/Scenes/PopularPersonsView/PopularPersonsView.swift
@@ -6,13 +6,65 @@
 //
 
 import SwiftUI
+import NetworkManager
 
 struct PopularPersonsView: View {
+    
+    // MARK: - Properties
+    
+    @StateObject var popularPersonsViewModel = PopularPersonsViewModel()
+    
+    @State var personsImage: Image = Image(systemName: "photo")
+    
+    private let gridColumns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("PopularPersonsView")
+        VStack {
+            HStack {
+                Text("Popular Persons")
+                    .font(.system(size: 22))
+                Spacer()
+            }
+            ScrollView {
+                LazyVGrid(columns: gridColumns, content: {
+                    ForEach(popularPersonsViewModel.popularPersons) { popularPersons in
+                        VStack(spacing: 8, content: {
+                            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(popularPersons.profilePath ?? "")")){ phase in
+                                switch phase {
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                case .failure:
+                                    Text("Image not found")
+                                case .empty:
+                                    Text("Loading...")
+                                @unknown default:
+                                    Text("Loading...")
+                                }
+                            }
+                            
+                            
+                            HStack {
+                                Text("\(popularPersons.name)")
+                                    .lineLimit(2)
+                                    .font(.system(size: 20))
+                                Spacer()
+                            }
+                        })
+                    }
+                })
+                .task {
+                    await popularPersonsViewModel.getPopularPersons()
+                }
+            }
+        }
     }
 }
 
 #Preview {
     PopularPersonsView()
 }
+
---
+++ MovieTracker/Scenes/PopularPersonsView/PopularPersonsViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  PopularPersonsViewModel.swift
+//  MovieTracker
+//
+//  Created by salome on 06.01.24.
+//
+
+import NetworkManager
+import Foundation
+
+final class PopularPersonsViewModel: ObservableObject {
+    //MARK: - Properties
+    @Published var popularPersons: [PopularPerson] = []
+    
+    private let networkManager = NetworkManager()
+
+    
+    
+    // MARK: - Network Calls
+    
+    init() {
+        Task {
+            await getPopularPersons()
+            print(popularPersons.count)
+        }
+    }
+    
+    func getPopularPersons() async {
+        let apiKey = "70dce981525cd8cfbfe0d94053fda913"
+        let urlString = "https://api.themoviedb.org/3/person/popular?api_key=\(apiKey)"
+        
+        do {
+            if let response: PopularPersonsResponse = try await networkManager.fetchData(fromURL: urlString) {
+                await MainActor.run {
+                    popularPersons = response.results
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
