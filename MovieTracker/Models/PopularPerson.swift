ccf8685795cce6474ff997f0e38860301c98bb60
salomege <salomegelenidze@gmail.com>
2024-01-07 00:37:46 -0800

    make first page

---
+++ MovieTracker/Models/PopularPerson.swift
@@ -0,0 +1,31 @@
+//
+//  PopularPerson.swift
+//  MovieTracker
+//
+//  Created by salome on 07.01.24.
+//
+
+import Foundation
+
+// MARK: - PopularPersonsResponse
+struct PopularPersonsResponse: Codable {
+    let results: [PopularPerson]
+
+}
+
+// MARK: - Result
+struct PopularPerson: Codable, Identifiable {
+    let adult: Bool
+      let id: Int
+    let name: String
+    let popularity: Double
+    let profilePath: String?
+
+    enum CodingKeys: String, CodingKey {
+        case adult, id
+        case name
+        case popularity
+        case profilePath = "profile_path"
+    }
+}
+
